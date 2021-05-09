e39b32af1a83e4b163155e47929c2a9688fcb7da
Kaustubh Iyer <kaustubhiyer1998@gmail.com>
2021-05-09 22:17:39 +0530

    complete

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+const cors = require("cors");
+const lyricsFinder = require("lyrics-finder");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "ea039643673c45e3961acfc5442caea1",
+    clientSecret: "01acfcbbfeed44bbbe2e855c9147969f",
+  });
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "ea039643673c45e3961acfc5442caea1",
+    clientSecret: "01acfcbbfeed44bbbe2e855c9147969f",
+    refreshToken,
+  });
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+      // spotifyApi.setAccessToken(data.body["access_token"]);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+app.get("/lyrics", async (req, res) => {
+  const lyrics =
+    (await lyricsFinder(req.query.artist, req.query.track)) ||
+    "No Lyrics Found";
+  res.json({ lyrics: lyrics });
+});
+
+app.listen(3001);
